483d52b5e7a9049185f5e073e4771f9e3134c850
BzlChina-1601A <956151773@qq.com>
2018-06-16 11:38:04 +0800

    提交

---
+++ src/components/home/myperson.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        我的
+    </div>
+</template>
+
+<script>
+import {getCookie} from '../../until/decode'
+export default {
+    mounted(){
+          let islogin =getCookie('token')
+           if(islogin){
+              // console.log(to)
+              // next()
+           }else{
+                this.$router.push({name:"login",query:{url:"myperson"}})
+           }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/home/shopcar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        购物车
+    </div>
+</template>
+
+<script>
+import {getCookie} from '../../until/decode.js'
+export default {
+    
+    data(){
+        return {
+
+        }
+    },
+    mounted(){
+         this.$http.post('http://localhost:3200/api/goodlist',{token:getCookie('token')}).then((res)=>{
+             if(res.code===10001){
+               console.log('1')
+             }else{
+                 this.$router.push({name:"login",query:{url:"shopcar"}})
+             }
+            // console.log(res)
+         })
+    }
+   
+   
+}
+</script>
+
+<style>
+
+</style>
