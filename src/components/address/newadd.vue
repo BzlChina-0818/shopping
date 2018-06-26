34627404b8ec6083c0fe12392e7be985b81d9583
BzlChina-1601A <956151773@qq.com>
2018-06-26 16:44:04 +0800

    打包

--- src/components/address/newadd.vue
+++ src/components/address/newadd.vue
@@ -16,25 +16,29 @@
                 <input type="number" placeholder="请输入手机号" v-model="phone">
                 </label>
           </p>
-          <p>
-              <select id="city">
-                  <option value="北京">北京</option>
-                  <option value="上海">上海</option>
-                  <option value="广东">广东</option>
-              </select>
-               <select id="citys">
-                  <option value="北京">北京</option>
-                  <option value="上海">上海</option>
-                  <option value="广东">广东</option>
-              </select>
-          </p>
-          <p>
-              <select id="area">
-                  <option value="海淀区">海淀区</option>
-                  <option value="丰田区">丰台区</option>
-                  <option value="朝阳区">朝阳区</option>
-              </select>
-          </p>
+         
+               <div style="display:flex"> 
+                   
+                          <multiselect
+                    v-model="selectpro"
+                  
+                    :options="provirce">
+                    </multiselect>
+                        <multiselect
+                    v-model="selectcity"
+                    :options="city">
+                    </multiselect>
+                   
+                  
+              </div>
+               
+         
+          <div>
+                 <multiselect
+                    v-model="selectarea"
+                    :options="area">
+                    </multiselect>
+          </div>
            <p>
                <input type="text" placeholder="详细地址" v-model="adress">
            </p>
@@ -47,46 +51,54 @@
 </template>
 
 <script>
-import {getCookie} from '../../until/decode.js'
+import 'vue-multiselect/dist/vue-multiselect.min.css';
+import Multiselect from "vue-multiselect";
+import { getCookie } from "../../until/decode.js";
 export default {
-     data(){
-                 return{
-                     username:"",
-                     phone:"",
-                     adress:"",
-                 }
-        },
-    methods:{
-       
-        backFn(){
-            this.$router.push({name:"adress"})
-        },
-        store(){
-            console.log()
-            this.$http.post('http://localhost:3200/api/adress',{
-                username:this.username,
-                phone:this.phone,
-                adress:this.adress,
-                city:city.value,
-                citys:citys.value,
-                area:area.value,
-                token:getCookie('token')
-
-            }).then((res)=>{
-                this.$router.go(-1)
-                  console.log('1')
-            })
-            
-        }
+  data() {
+    return {
+      username: "",
+      phone: "",
+      adress: "",
+      selectpro:"",
+      selectcity:"",
+      selectarea:"",
+      provirce:['上海',"北京","广州"],
+      city:["上海","北京",'广州'],
+      area:["海淀区","朝阳区","大兴区"]
+    };
+  },
+     components: { Multiselect },
+  methods: {
+    backFn() {
+      this.$router.push({ name: "adress" });
+    },
+    store() {
+      console.log();
+      this.$http
+        .post("http://localhost:3200/api/adress", {
+          username: this.username,
+          phone: this.phone,
+          adress: this.adress,
+          city: this.selectpro,
+          citys: this.selectcity,
+          area: this.selectarea,
+          token: getCookie("token")
+        })
+        .then(res => {
+          this.$router.go(-1);
+          console.log("1");
+        });
     }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.wraps{
-    width:100%;
-    height:100%;
-    background: #f3f3f3;
+.wraps {
+  width: 100%;
+  height: 100%;
+  background: #f3f3f3;
 }
 header {
   height: 0.9rem;
@@ -95,35 +107,34 @@ header {
   padding: 0 0.2rem;
   justify-content: space-between;
 }
-section{
-    p{
-        height:1.2rem;
-        padding: 0 .2rem;
-        input{
-            width: 100%;
-            height: .9rem;
-            outline: none;
-            border:0;
-            background: #fff;
-        }
+section {
+  p {
+    height: 1.2rem;
+    padding: 0 0.2rem;
+    input {
+      width: 100%;
+      height: 0.9rem;
+      outline: none;
+      border: 0;
+      background: #fff;
     }
+  }
 }
-.footer{
-    height: 1.7rem;
-
+.footer {
+  height: 1.7rem;
 }
-.footer button{
-    width:4.6rem;
-    height: .97rem;
-    background: #fc4141;
-    color:#fff;
-    text-align: center;
-    line-height: .97rem;
-    font-size: .3rem;
-    margin:.5rem auto;
-    display: block;
-    border: none;
-    outline: none;
-    border-radius: 25px;
+.footer button {
+  width: 4.6rem;
+  height: 0.97rem;
+  background: #fc4141;
+  color: #fff;
+  text-align: center;
+  line-height: 0.97rem;
+  font-size: 0.3rem;
+  margin: 0.5rem auto;
+  display: block;
+  border: none;
+  outline: none;
+  border-radius: 25px;
 }
 </style>
